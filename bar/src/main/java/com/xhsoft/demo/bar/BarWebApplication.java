3b8c641876523a549fe3a37dfa5621061ccec072
张晓辉 <zhangxh@zhangxiaohuideMacBook-Pro.local>
2016-05-28 11:44:20 +0800

    为 bar 单独添加一个运行配置

---
+++ bar/src/main/java/com/xhsoft/demo/bar/BarWebApplication.java
@@ -0,0 +1,12 @@
+package com.xhsoft.demo.bar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarWebApplication.class, args);
+	}
+}
