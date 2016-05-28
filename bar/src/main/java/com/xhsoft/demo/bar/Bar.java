1afe4418aa01a2de8133ac6e043613916bc4c980
张晓辉 <zhangxh@zhangxiaohuideMacBook-Pro.local>
2016-05-28 10:16:07 +0800

    实现spring boot 初步运行
    两个模块 foo 和 bar，外加一个独立的web应用，web依赖于 foo 和 bar

---
+++ bar/src/main/java/com/xhsoft/demo/bar/Bar.java
@@ -0,0 +1,19 @@
+package com.xhsoft.demo.bar;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 测试 Controller
+ */
+@Controller
+public class Bar {
+
+    @RequestMapping("/bar")
+    public String hello(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
