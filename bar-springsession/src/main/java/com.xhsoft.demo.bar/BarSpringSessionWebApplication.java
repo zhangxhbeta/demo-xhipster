2d6914ae9ac97250efad6f1da8ec4546c5ec5f2e
张晓辉 <zhangxh@zhangxiaohuideMacBook-Pro.local>
2016-05-31 10:48:37 +0800

    复制一份 bar-springsession
    然后进行测试 session 的集成，也就是 单点登录

---
+++ bar-springsession/src/main/java/com.xhsoft.demo.bar/BarSpringSessionWebApplication.java
@@ -0,0 +1,51 @@
+package com.xhsoft.demo.bar;
+
+
+import com.xhsoft.demo.foo.cofig.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
+
+import javax.inject.Inject;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+public class BarSpringSessionWebApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(BarSpringSessionWebApplication.class);
+
+    @Inject
+    private Environment env;
+
+    /**
+     * Main method, used to run the application.
+     */
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication app = new SpringApplication(BarSpringSessionWebApplication.class);
+        SimpleCommandLinePropertySource source = new SimpleCommandLinePropertySource(args);
+        addDefaultProfile(app, source);
+        Environment env = app.run(args).getEnvironment();
+        log.info("Access URLs:\n----------------------------------------------------------\n\t" +
+                        "Local: \t\thttp://127.0.0.1:{}\n\t" +
+                        "External: \thttp://{}:{}\n----------------------------------------------------------",
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"));
+
+    }
+
+    /**
+     * If no profile has been configured, set by default the "dev" profile.
+     */
+    private static void addDefaultProfile(SpringApplication app, SimpleCommandLinePropertySource source) {
+        if (!source.containsProperty("spring.profiles.active") &&
+                !System.getenv().containsKey("SPRING_PROFILES_ACTIVE")) {
+
+            app.setAdditionalProfiles(Constants.SPRING_PROFILE_DEVELOPMENT);
+        }
+    }
+}
---
+++ bar-springsession/src/main/java/com.xhsoft.demo.bar/cofig/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.xhsoft.demo.bar.cofig;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.authentication.builders.*;
+import org.springframework.security.config.annotation.web.configuration.*;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("admin").roles("USER");
+    }
+}
